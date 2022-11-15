5d9c4cc2566426d3f8c7184b1d83dd049b86492e
your name <your@email.com>
2022-11-15 18:22:31 -0300

    Add animation

--- client/src/components/Emojis/Emoji/Emoji.js
+++ client/src/components/Emojis/Emoji/Emoji.js
@@ -9,10 +9,11 @@ import {
   Button,
   Typography,
   Grid,
+  Zoom,
+  Grow
 } from "@material-ui/core/";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import moment from "moment";
 
 import { likeEmoji, deleteEmoji } from "../../../actions/emojis";
 import useStyles from "./emoji_style";
@@ -39,47 +40,49 @@ const Emoji = ({ emoji, setCurrentId }) => {
   const openEmoji = (e) => history(`/emojis/${emoji._id}`);
 
   return (
-    <Card className={classes.card} elevation={6} >
-      <Grid 
-        container
-        spacing={3}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: "35vh" }}
-      >
-        <Typography gutterBottom variant="h2" component="div">
-          {emoji.emoji}
-        </Typography>
-
-        <Typography gutterBottom variant="h6" component="div">
-          {emoji.name}
-        </Typography>
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            Votes: {emoji.likes}
+    <Zoom in style={{ transitionDelay: "200ms" }}>
+      <Card className={classes.card} elevation={6}>
+        <Grid
+          container
+          spacing={3}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "35vh" }}
+        >
+          <Typography gutterBottom variant="h2" component="div">
+            {emoji.emoji}
           </Typography>
-        </CardContent>
-        <CardActions>
-          <Button
-            size="small"
-            color="secondary"
-            variant="contained"
-            onClick={() => dispatch(likeEmoji(emoji._id))}
-          >
-            Vote
-          </Button>
-          <Button
-            size="small"
-            color="primary"
-            variant="contained"
-            onClick={openEmoji}
-          >
-            Show
-          </Button>
-        </CardActions>
-      </Grid>
-    </Card>
+
+          <Typography gutterBottom variant="h6" component="div">
+            {emoji.name}
+          </Typography>
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              Votes: {emoji.likes}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              color="secondary"
+              variant="contained"
+              onClick={() => dispatch(likeEmoji(emoji._id))}
+            >
+              Vote
+            </Button>
+            <Button
+              size="small"
+              color="primary"
+              variant="contained"
+              onClick={openEmoji}
+            >
+              Show
+            </Button>
+          </CardActions>
+        </Grid>
+      </Card>
+    </Zoom>
   );
 };
 
